246e275ee762fbcfc468db115f81f6ccbe00fde9
Nicolas MERELLI <nicolas.merelli@gmail.com>
2016-07-30 23:43:23 +0200

    feat(unrestrict): ajout du check d'un lien

--- lib/real_debrid/unrestrict.rb
+++ lib/real_debrid/unrestrict.rb
@@ -11,12 +11,20 @@ class RealDebrid
     #
     # @param [String] link Ressource sur laquelle effectuer la requête en provenance de Real-debrid
     # @param [String] password Corps de la requête
-    # @param [Int] remote Corps de la requête
+    # @return [RealDebrid::Response] Réponse en provenance de Real-debrid
+    def check(link, password = nil)
+      params = { 'link': link, 'password': password }
+      post_request URL_SUFFIX_BASE + '/check', params
+    end
+
+    # Effetue une requête de débridage d'un lien
+    #
+    # @param [String] link Ressource sur laquelle effectuer la requête en provenance de Real-debrid
+    # @param [String] password Corps de la requête
+    # @param [Int] remote 0 ou 1, Utilisation du trafic remote
     # @return [RealDebrid::Response] Réponse en provenance de Real-debrid
     def link(link, password = nil, remote = 0)
-      params = {}
-      params['link'] = link
-      params['remote'] = remote
+      params = { 'link': link, 'remote': remote }
       params['password'] = password if password
       post_request URL_SUFFIX_BASE + '/link', params
     end
